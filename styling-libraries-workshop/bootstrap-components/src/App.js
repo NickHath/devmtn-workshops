c2c2e4e13c2571473c3386c8f3f6fb8d1afbe896
Nick Hathaway <Nicholashathaway@gmail.com>
2018-08-18 18:22:06 -0600

    clone form demo with reactstrap

--- styling-libraries-workshop/bootstrap-components/src/App.js
+++ styling-libraries-workshop/bootstrap-components/src/App.js
@@ -1,18 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+// bootstrap styles have been imported in index.js
+// react-bootstrap components
+import { 
+  Card, CardBody, CardTitle, 
+  CardText, Button, Form, 
+  FormText, FormGroup, Label, 
+  Input, Col
+} from 'reactstrap';
 
 class App extends Component {
   render() {
+    // use inline styles to override bootstrap styling  
+    const styles = {
+      cardStyle: { width: '50%', margin: 'auto' },
+      colStyle: { padding: '0', margin: '0' },
+      checkBoxStyle: { marginBottom: '1rem' }
+    };
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+        <Card style={ styles.cardStyle }>
+          <CardBody>
+            {/* CARD DETAILS */}
+            <CardTitle>Sign up</CardTitle>
+            <CardText>Please enter your personal information below.</CardText>
+            {/* FORM */}
+            <Form>
+              <FormGroup>
+                <Label>Name</Label>
+                <Form inline>
+                  <Col style={ styles.colStyle }>
+                    <Input placeholder="First name" />
+                  </Col>
+                  <Col style={ styles.colStyle }>
+                    <Input placeholder="Last name" />
+                  </Col>
+                </Form>
+              </FormGroup>
+              <FormGroup>
+                <Label>Email address</Label>
+                <Input placeholder="Enter email" />
+                <FormText color="danger">We'll never share your email with anyone else.</FormText>
+              </FormGroup>
+              <FormGroup>
+                <Label>Password</Label>
+                <Input placeholder="Password" />
+              </FormGroup>
+              <FormGroup check style={ styles.checkBoxStyle }>
+                <Label check>
+                  <Input type="checkbox" />
+                    I agree to your 1,000,000 page privacy policy.
+                  </Label>
+              </FormGroup>
+            </Form>
+            {/* BUTTONS */}
+            <Button color="primary">Submit</Button> {' '}
+            <Button outline color="primary">Cancel</Button>
+          </CardBody>
+        </Card>
       </div>
     );
   }
